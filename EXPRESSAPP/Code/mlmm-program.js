3cf75a3b10dffd58651bcc4b2af40973df69ea46
bhalla08 <bhalla.varun801@gmail.com>
2023-11-17 00:30:56 +0530

    modification1-mlmm-program

--- EXPRESSAPP/Code/mlmm-program.js
+++ EXPRESSAPP/Code/mlmm-program.js
@@ -2,26 +2,24 @@ const mlmmProgram = (inputs) =>{
 
     const answer = [];
   
-    for (const input of inputs) {
-      const cutoff = parseInt(input[0]);
-      const numScores = parseInt(input[1]);
-      const scores = input[2].split(' ').map(Number);
-  
-      let currentSum = 0;
-      let count = 0;
-  
-      for (const score of scores) {
-        currentSum += score;
-  
-        if (currentSum < cutoff) {
-          count++;
-        } else {
-          currentSum = 0;
-        }
+     for (const input of inputs) {
+    const cutoff = parseInt(input[0]);
+    const numScores = parseInt(input[1]);
+    const scores = input[2].split(' ').map(Number);
+
+    let currentSum = 0;
+    let count = 0;
+
+    for (const score of scores) {
+      currentSum = Math.max(0, currentSum + score);
+
+      if (currentSum < cutoff) {
+        count++;
       }
-  
-      answer.push(count);
     }
+
+    answer.push(count);
+  }
     return answer;
   
   }
